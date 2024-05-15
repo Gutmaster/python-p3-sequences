5fc1756c33f44bea9c24562a8adc65b6da648e9e
+ <+>
2024-05-14 17:16:05 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    for i in range(length):
+        if i < 2:
+            fibonacci.append(i)
+        else:
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    print(fibonacci)
